29f600dfde90e96d6fd50c1e2b243a8b5f680b44
mayra lucia navarro <luciagirasoles@gmail.com>
2019-12-12 01:44:58 -0500

    update waiter view with current order

--- app/javascript/src/views/Waiter.jsx
+++ app/javascript/src/views/Waiter.jsx
@@ -3,8 +3,23 @@ import Footer from "../components/Footer";
 import style from "./Waiter.module.css";
 import iconHome from "../images/home.svg";
 import { navigate } from "@reach/router";
+import { useOrdersReady } from "../../redux/selectors/index";
+import Modal from "../components/Modal";
 
 export default function Waiter() {
+  const [showModal, setShowModal] = React.useState(false);
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  const orders = useOrdersReady();
+  function handleOnclick(event) {
+    console.dir(event.target);
+    console.log(event.target.parentNode);
+  }
   return (
     <div className={style.main}>
       <img
@@ -15,13 +30,45 @@ export default function Waiter() {
         }}
         alt="back home"
       />
-      <h2 className={style.title}>Ordenes</h2>
-      <ul>
-        <li>Orden 1</li>
-        <li>Orden 2</li>
-        <li>Orden 3</li>
-      </ul>
+      <div className={style.ready}>
+        <h3>{Object.keys(orders).length}</h3>
+      </div>
+      <h2 className={style.title}>Ordenes listas</h2>
+      {!!Object.keys(orders).length ? (
+        <div className={style.ordersSection}>
+          {Object.values(orders).map(order => {
+            return (
+              <section
+                key={order.id.toString()}
+                className={`${style.card} ${style.color_amber}`}
+              >
+                <h5>#0{order.id}</h5>
+                <p>{`entrada: ${order.starter.name}`}</p>
+                <p>{`segundo: ${order.mainCourse.name}`}</p>
+                <p>{`bebida: ${order.beverage.name}`}</p>
+                <UpdateButton orderId={order.id} onClick={openModal} />
+              </section>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Sin ordenes para entregar</p>
+      )}
+      {showModal && (
+        <Modal>
+          <h3>{`La orden 0${order} `}</h3>
+        </Modal>
+      )}
+
       <Footer role="waiter" />
     </div>
+  );
+}
+
+function UpdateButton({ orderId, closeModal }) {
+  return (
+    <>
+      <button onClick={handleOnclick}>Entregado</button>
+    </>
   );
 }
