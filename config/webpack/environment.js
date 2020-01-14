7ef4e3c59a6c60ca638bd045d1b32af110d30bd9
mayra lucia navarro <luciagirasoles@gmail.com>
2020-01-14 13:37:07 -0500

    delete modeModules in webpack environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,4 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-module.exports = environment
+module.exports = environment;
+environment.loaders.delete("nodeModules");
